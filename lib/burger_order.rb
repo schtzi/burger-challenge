c7f084038f0a097d6c4c6fea526d0714ea34fc60
Jan Schuetz <janalexschuetz@gmail.com>
2023-02-16 10:14:55 +0100

    Finished 2nd implementation

--- lib/burger_order.rb
+++ lib/burger_order.rb
@@ -2,6 +2,12 @@ require 'json'
 require 'pry-byebug'
 
 module BurgerOrder
+  # before_action :load_database, only: :calculate_total_price
+
+  # def self.load_database
+  #   @@data = Data.new
+  # end
+
   class Data
     attr_reader :burgers, :size_multipliers, :ingredients, :promotions, :discounts
 
@@ -18,6 +24,7 @@ module BurgerOrder
 
   class Order
     attr_reader :content, :promotions, :discount
+    attr_accessor :total_value
 
     def initialize(order_file)
       order_data = JSON.parse(order_file)
@@ -29,10 +36,7 @@ module BurgerOrder
 
       @promotions = order_data['promotion_codes']
       @discount = order_data['discount_code']
-    end
-
-    def total_value
-      @content.sum(&:extras_price) + @content.sum(&:burger_price)
+      @total_value = @content.sum(&:extras_price) + @content.sum(&:burger_price)
     end
   end
 
@@ -40,6 +44,7 @@ module BurgerOrder
     attr_accessor :name, :size, :add, :remove, :burger_price, :extras_price
 
     def initialize(attributes)
+      @data = Data.new # needs to be refactored so that it's only done once
       @name = attributes[:name]
       @size = attributes[:size]
       @add = attributes[:add]
@@ -49,8 +54,6 @@ module BurgerOrder
     end
 
     def add_prices
-      @data = Data.new
-
       @burger_price = @data.burgers[@name] * @data.size_multipliers[@size]
       @extras_price = 0
 
@@ -66,29 +69,40 @@ module BurgerOrder
     def self.all
       ObjectSpace.each_object(self).to_a
     end
-
-    def self.count
-      all.count
-    end
   end
 
   def self.calculate_total_price(order_file)
-    data = Data.new
     order = Order.new(order_file)
-    order.discount.any? ? apply_discount(order, data) : order.total_value
+    data = Data.new # needs to be refactored so that it's only done once
 
-    # apply_discount = (order.sum(&:extras_price) + price_after_promo(order.content))
+    order.promotions.nil? ? order.total_value : apply_promotions(order, data)
+    order.discount.empty? ? order.total_value : apply_discount(order, data)
+
+  end
+
+  def self.apply_promotions(order, data)
+    order.promotions.each do |promo|
+      promo_data = data.promotions[promo]
+
+      count = Burger.all.count { |b| b.name == promo_data['target'] && b.size == promo_data['target_size'] }
+      relevant_count = count - (count % promo_data['from'])
+      number_of_possible_promotions = relevant_count / promo_data['from']
+      reduction = number_of_possible_promotions / (promo_data['from'] - promo_data['to'])
+
+      order.total_value -= (reduction * data.burgers[promo_data['target']] * data.size_multipliers[promo_data['target_size']])
+    end
+    order.total_value.round(2)
   end
 
   def self.apply_discount(order, data)
     discount_data = data.discounts[order.discount]
-    case discount_data
+    case discount_data.keys[0]
     when 'deduction_in_percent'
-      order.total_value * (1 - discount_data['deduction_in_percent'] / 100)
+      order.total_value = (order.total_value * (1 - (discount_data['deduction_in_percent'] / 100.0))).round(2)
     when 'deduction_in_euro'
-      order.total_value - discount_data['deduction_in_euro']
+      order.total_value -= discount_data['deduction_in_euro']
     when 'deduction_in_cents'
-      order.total_value - (discount_data['deduction_in_euro'] / 100)
+      order.total_value -= (discount_data['deduction_in_euro'] / 100)
     else
       order.total_value
     end
--- spec/burger_order_spec.rb
+++ spec/burger_order_spec.rb
@@ -1,8 +1,13 @@
 require "spec_helper"
 
 RSpec.describe BurgerOrder do
-  it "calculates the prices for an order" do
+  it "calculates the prices for a finstreet-order" do
     order_file = File.absolute_path("spec/fixtures/order.json")
     expect(BurgerOrder.calculate_total_price(order_file)).to eq(16.29)
+  end
+
+  it "calculates the price for 4 hamburgers no specials" do
+    order_file = File.absolute_path("spec/fixtures/order_2.json")
+    expect(BurgerOrder.calculate_total_price(order_file)).to eq(14.00)
   end
 end
